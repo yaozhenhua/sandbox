fcd399afa7aa6a0174754f975191c15ed623e17a
Zhenhua Yao <zhyao@microsoft.com>
2024-04-02 17:38:05 -0700

    Finish the async DNS query code sample
    Squashed commit of the following:
    
    commit 13215fdfc79b072d77d823466de75c288cd2a8c0
    Author: Zhenhua Yao <zhyao@microsoft.com>
    Date:   Tue Apr 2 17:37:29 2024 -0700
    
        final result
    
    commit c110130f9be5cc01f4605f974b17628876b5027a
    Author: Zhenhua Yao <Yao.Zhenhua@microsoft.com>
    Date:   Tue Apr 2 17:20:59 2024 -0700
    
        fix the bug
    
    commit 45f688b559757fa389c49f94c5907a58122a3b37
    Author: Zhenhua Yao <zhyao@microsoft.com>
    Date:   Tue Apr 2 16:45:04 2024 -0700
    
        temporary change

--- AsyncDnsQuery/AsyncDnsQuery.cs
+++ AsyncDnsQuery/AsyncDnsQuery.cs
@@ -1,9 +1,18 @@
+// Compile this code
+//      csc /platform:x64 /debug AsyncDnsQuery.cs
+//
+// On ARM64 Windows:
+//      csc /platform:arm64 /debug AsyncDnsQuery.cs
+
 namespace Test
 {
     using System;
     using System.Runtime.InteropServices;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Asynchronous DNS query result to dispose native resource.
+    /// </summary>
     public sealed class DnsQueryResult : IDisposable
     {
         private readonly IntPtr contextPtr;
@@ -15,25 +24,54 @@ namespace Test
             this.contextPtr = contextPtr;
         }
 
+        /// <summary>
+        /// Gets the status code of the DNS query.
+        /// </summary>
         public int Code { get; }
+
+        /// <summary>
+        /// Gets the return message, or the query result of the DNS query.
+        /// </summary>
         public string Message { get; }
 
+        /// <inheritdoc />
         public void Dispose()
         {
+            // Release the pinned resource.
             GCHandle.FromIntPtr(this.contextPtr).Free();
             GC.SuppressFinalize(this);
 
-            Console.WriteLine("GC handle freed");
+            // Console.WriteLine("GC handle freed");
         }
     }
 
+    /// <summary>
+    /// Queries the DNS using native Win32 API asynchronously.
+    /// </summary>
     public static class AsyncDnsQuery
     {
+        /// <summary>
+        /// Types of DNS record type supported by the method.
+        /// </summary>
+        public enum DnsRecordType : ushort
+        {
+            DNS_TYPE_A = 0x1,
+            DNS_TYPE_CNAME = 0x5,
+            DNS_TYPE_AAAA = 0x1C,
+            DNS_TYPE_SRV = 0x21,
+        }
+
+        /// <summary>
+        /// Internal callback function passed to the native code.
+        /// </summary>
         private delegate void OnQueryFinished(
             int returnCode,
             [MarshalAs(UnmanagedType.LPWStr)] string message,
             IntPtr context);
 
+        /// <summary>
+        /// Internal function defined in the native code to start the DNS query.
+        /// </summary>
         [DllImport(
             "DnsQueryWin",
             CharSet = CharSet.Unicode,
@@ -41,19 +79,31 @@ namespace Test
             CallingConvention = CallingConvention.StdCall)]
         private static extern int QueryDns(
             string hostName,
+            DnsRecordType queryType,
             [MarshalAs(UnmanagedType.FunctionPtr)] OnQueryFinished callback,
             IntPtr context);
 
-        public static Task<DnsQueryResult> QueryAsync(string hostName)
+        /// <summary>
+        /// Queries DNS server using Win32 API asynchronously.
+        /// </summary>
+        /// <param name="hostName">Host name to be queried.</param>
+        /// <param name="queryType">DNS query type.</param>
+        /// <returns>Async task that can be resolved to the query result on completion.</returns>
+        public static Task<DnsQueryResult> QueryAsync(string hostName, DnsRecordType queryType)
         {
             var tcs = new TaskCompletionSource<DnsQueryResult>();
+
+            // Pin the object so it will not move during GC.
             var contextPtr = GCHandle.ToIntPtr(GCHandle.Alloc(tcs));
 
-            AsyncDnsQuery.QueryDns(hostName, AsyncDnsQuery.MyOnQueryFinished, contextPtr);
+            AsyncDnsQuery.QueryDns(hostName, queryType, AsyncDnsQuery.MyOnQueryFinished, contextPtr);
 
             return tcs.Task;
         }
 
+        /// <summary>
+        /// Callback that passed to the native function. This will finish the async task.
+        /// </summary>
         private static void MyOnQueryFinished(int returnCode, string message, IntPtr context)
         {
             var ret = (TaskCompletionSource<DnsQueryResult>)GCHandle.FromIntPtr(context).Target;
@@ -62,12 +112,28 @@ namespace Test
         }
     }
 
+    /// <summary>
+    /// Sample test code.
+    /// </summary>
     internal sealed class Test
     {
-        private static async Task Main()
+        private static async Task Main(string[] argv)
         {
-            using var ret = await AsyncDnsQuery.QueryAsync("www.google.com");
-            Console.WriteLine($"Result: {ret.Code} {ret.Message}");
+            var hostname = argv.Length > 0 ? argv[0] : "www.bing.com";
+
+            var ret = await AsyncDnsQuery.QueryAsync(hostname, AsyncDnsQuery.DnsRecordType.DNS_TYPE_A);
+            Console.WriteLine($"type=A: status={ret.Code} result={ret.Message}\n");
+
+            ret = await AsyncDnsQuery.QueryAsync(hostname, AsyncDnsQuery.DnsRecordType.DNS_TYPE_AAAA);
+            Console.WriteLine($"type=AAAA: status={ret.Code} result={ret.Message}\n");
+
+            ret = await AsyncDnsQuery.QueryAsync(hostname, AsyncDnsQuery.DnsRecordType.DNS_TYPE_CNAME);
+            Console.WriteLine($"type=CNAME: status={ret.Code} result={ret.Message}\n");
+
+            ret = await AsyncDnsQuery.QueryAsync(hostname, AsyncDnsQuery.DnsRecordType.DNS_TYPE_SRV);
+            Console.WriteLine($"type=SRV: status={ret.Code} result={ret.Message}\n");
+
+            ret.Dispose();
         }
     }
 }
--- AsyncDnsQuery/DnsQueryWin.cpp
+++ AsyncDnsQuery/DnsQueryWin.cpp
@@ -1,10 +1,11 @@
-// Build a DLL: cl DnsQueryWin.cpp /LD /W3 /O2 /Zi
+// Build a DLL: cl DnsQueryWin.cpp /EHsc /LD /W3 /O2 /Zi
 #ifndef UNICODE
 #define UNICODE
 #endif
 
 #define WIN32_LEAN_AND_MEAN
 
+#include <iostream>
 #include <string>
 
 #include <windows.h>
@@ -19,16 +20,136 @@ extern "C"
     // Callback provided by the client, which will be invoked when the query is finished.
     typedef void (__stdcall *OnQueryFinished)(int, const wchar_t*, void*);
 
+    // Information saved in the query start, and will be used in the callback.
+    struct QueryContext
+    {
+        OnQueryFinished     Callback;
+        void*               Context;
+        DNS_QUERY_REQUEST   QueryRequest;
+        DNS_QUERY_RESULT    QueryResult;
+    };
+
+    // Callback to DnsQueryEx internally.
+    void CALLBACK InternalQueryCompletionCallback(
+        _In_ PVOID pQueryContext,
+        _Inout_ PDNS_QUERY_RESULT pQueryResults)
+    {
+        DNS_STATUS status = pQueryResults->QueryStatus;
+
+        QueryContext* queryContext = static_cast<QueryContext*>(pQueryContext);
+
+        // Query result contains all resolved addresses and TTL, separated by "=". Different records are delimited
+        // by semicolon.
+        std::wstring resolvedAddresses;
+        
+        if (status == ERROR_SUCCESS)
+        {
+            // Preserve the buffer to avoid excessive reallocation.
+            resolvedAddresses.reserve(1024);
+
+            PDNS_RECORD pDnsRecord = pQueryResults->pQueryRecords;
+            while (pDnsRecord)
+            {
+                // Maximum length of IPv6 string.
+                WCHAR buffer[65] = L"\0";
+
+                if (resolvedAddresses.size())
+                {
+                    resolvedAddresses += L";";
+                }
+
+                switch (pDnsRecord->wType)
+                {
+                case DNS_TYPE_A:
+                    InetNtop(AF_INET, (PVOID)&pDnsRecord->Data.A.IpAddress, buffer, INET_ADDRSTRLEN);
+                    // std::wcout << L"A Record: " << buffer << std::endl;
+                    resolvedAddresses += buffer;
+
+                    break;
+
+                case DNS_TYPE_AAAA:
+                    InetNtop(AF_INET6, (PVOID)&pDnsRecord->Data.AAAA.Ip6Address, buffer, INET6_ADDRSTRLEN);
+                    // std::wcout << L"AAAA Record: " << buffer << std::endl;
+                    resolvedAddresses += buffer;
+
+                    break;
+
+                case DNS_TYPE_CNAME:
+                    // std::wcout << L"CNAME: " << pDnsRecord->Data.Cname.pNameHost << std::endl;
+                    resolvedAddresses += pDnsRecord->Data.Cname.pNameHost;
+
+                    break;
+
+                case DNS_TYPE_SRV:
+                    // std::wcout << L"SRV Record: " << pDnsRecord->Data.SRV.pNameTarget
+                    //     << L" Priority: " << pDnsRecord->Data.SRV.wPriority
+                    //     << L" Weight: " << pDnsRecord->Data.SRV.wWeight
+                    //     << L" Port: " << pDnsRecord->Data.SRV.wPort << std::endl;
+                    resolvedAddresses += pDnsRecord->Data.SRV.pNameTarget;
+
+                    break;
+
+                default:
+                    // std::wcout << L"Other Record Type: " << pDnsRecord->wType << std::endl;
+                    break;
+                }
+
+                // std::wcout << L"TTL: " << pDnsRecord->dwTtl << L" seconds" << std::endl;
+                resolvedAddresses += L"=";
+                resolvedAddresses += std::to_wstring(pDnsRecord->dwTtl);
+
+                pDnsRecord = pDnsRecord->pNext;
+            }
+        }
+        // else
+        // {
+        //     std::wcerr << L"DNS query failed with status: " << status << std::endl;
+        // }
+
+        // Send the query result and status to the managed code.
+        queryContext->Callback(status, resolvedAddresses.c_str(), queryContext->Context);
+
+        if (pQueryResults->pQueryRecords)
+        {
+            DnsRecordListFree(pQueryResults->pQueryRecords, DnsFreeRecordList);
+        }
+
+        delete queryContext;
+
+        // std::wcout << L"returned " << resolvedAddresses << L"." << std::endl;
+    }
+
     __declspec(dllexport) int __stdcall QueryDns(
-        _In_ const wchar_t* hostName,
+        _In_ PCWSTR hostName,
+        _In_ WORD wType,
         _In_ OnQueryFinished callback,
         _In_ void* context)
     {
-        std::wstring str(L"abc: ");
+        DNS_STATUS dnsStatus = 0;
+        QueryContext* queryContext = new QueryContext();
+        ZeroMemory(queryContext, sizeof(QueryContext));
 
-        str += hostName;
+        queryContext->Callback = callback;
+        queryContext->Context = context;
 
-        callback(10, str.c_str(), context);
+        // Initialize the DNS_QUERY_REQUEST structure
+        queryContext->QueryRequest.Version = DNS_QUERY_REQUEST_VERSION1;
+        queryContext->QueryRequest.QueryName = hostName;
+        queryContext->QueryRequest.QueryType = wType;
+        queryContext->QueryRequest.QueryOptions = DNS_QUERY_STANDARD;
+        queryContext->QueryRequest.pDnsServerList = NULL;
+        queryContext->QueryRequest.InterfaceIndex = 0;
+        queryContext->QueryRequest.pQueryCompletionCallback = InternalQueryCompletionCallback;
+        queryContext->QueryRequest.pQueryContext = queryContext;
+
+        // Initialize the DNS_QUERY_RESULT structure
+        queryContext->QueryResult.Version = DNS_QUERY_REQUEST_VERSION1;
+
+        dnsStatus = DnsQueryEx(&(queryContext->QueryRequest), &(queryContext->QueryResult), NULL);
+        if (dnsStatus != DNS_REQUEST_PENDING)
+        {
+            InternalQueryCompletionCallback(queryContext, &(queryContext->QueryResult));
+        }
 
         return 0;
     }
